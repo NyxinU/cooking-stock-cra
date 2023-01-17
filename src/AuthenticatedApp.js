70c5e3a0bdc559595050d3dd131dd1e4ebf85c74
NyxinU <yiunixon@gmail.com>
2023-01-17 12:16:28 -0800

    delete random recipe

--- src/AuthenticatedApp.js
+++ src/AuthenticatedApp.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import DiscoverRecipies from "./DiscoverRecipes/DiscoverRecipes";
 import Recipe from "./Recipe/Recipe";
-import RandomRecipe from "./RandomRecipe/RandomRecipe";
 
 export default function AuthenticatedApp() {
   return <AppRoutes />;
@@ -11,7 +10,6 @@ export default function AuthenticatedApp() {
 function AppRoutes({ user }) {
   return (
     <Routes>
-      <Route path="/recipes/random" element={<RandomRecipe />} />
       <Route path="/recipes/:id" element={<Recipe />} />
       <Route path="/" element={<DiscoverRecipies />} />
     </Routes>
